9acd80ba6e47688b7c41c3f566c1961aabe1822b
jossAcosta <josselineacostamtz@gmail.com>
2020-03-15 18:35:53 -0600

    Read notes of firestore in dashboard view

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -7,7 +7,36 @@ import firebase from '../firebase.js';
 
 
 class Dashboard extends Component {
-
+    constructor(props) {
+        super(props);
+        this.ref = firebase.firestore().collection('note');
+        this.unsubscribe = null;
+        this.state = {
+          note: []
+        };
+      }
+     
+      onCollectionUpdate = (querySnapshot) => {
+        const note= [];
+        querySnapshot.forEach((doc) => {
+          const { title, description, author } = doc.data();
+          note.push({
+            key: doc.id,
+            doc, // DocumentSnapshot
+            title,
+            description,
+            author,
+          });
+        });
+        this.setState({
+          note
+       });
+      }
+     
+      componentDidMount() {
+        this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
+      }
+    
     render(){   
         const user = this.props
         return (
@@ -19,7 +48,22 @@ class Dashboard extends Component {
                     <div className=" row note_buttons"> 
                         <Link to="/notes/new" className="btn ">New Note </Link>
                     </div>
-                    <div className="row"></div>
+                    <div className="row">
+                    <div className="">
+
+            {this.state.note.map((singleNote)=>{
+                return (
+                    <ul className ="single_note list-unstyled ">
+                    <li className="singleNote_title">{singleNote.title}</li>
+                    <li className="singleNote_body">{singleNote.description}</li>
+                    <li>{singleNote.author}</li>
+                    </ul>
+                )
+            })}
+        </div>
+             
+
+                    </div>
                 </div>
            </div>
         );

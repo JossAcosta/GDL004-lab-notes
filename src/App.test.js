a7b574e1ae639b191a4bc2cfa44bb70ac87115f0
jossAcosta <josselineacostamtz@gmail.com>
2020-04-28 18:36:19 -0500

    deploy

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App.js';
+import App from '../src/App.js'
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/notes/i);
+  const linkElement = getByText(/LOG/);
   expect(linkElement).toBeInTheDocument();
 });

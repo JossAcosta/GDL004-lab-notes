74116aa09ddc5126e407e3f42c34526eae716328
jossAcosta <josselineacostamtz@gmail.com>
2020-04-28 18:24:30 -0500

    deploy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import App2 from './app';
+import App from './App';
 
 const container = document.getElementById('app');
-ReactDOM.render(  <App2 />,container);
+ReactDOM.render(  <App />,container);
 {/* <Router> */}
    
 // </Router>

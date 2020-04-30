65b38b03ca3a0133dff7f02afc0e76b1482e03dc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-30 23:25:16 +0000

    Deploy JossAcosta/GDL004-lab-notes to github.com/JossAcosta/GDL004-lab-notes.git:gh-pages

---
+++ precache-manifest.34003e23b595d1acc4714299a6ba57ba.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "049bd733933026e5c3f2d5224d41cfcb",
+    "url": "/GDL004-lab-notes/index.html"
+  },
+  {
+    "revision": "6d1ab3bb05b5d061f059",
+    "url": "/GDL004-lab-notes/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "7017ee769b9cbab9c46b",
+    "url": "/GDL004-lab-notes/static/css/main.d1980e7f.chunk.css"
+  },
+  {
+    "revision": "6d1ab3bb05b5d061f059",
+    "url": "/GDL004-lab-notes/static/js/2.063eedee.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/GDL004-lab-notes/static/js/2.063eedee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7017ee769b9cbab9c46b",
+    "url": "/GDL004-lab-notes/static/js/main.23ce2f02.chunk.js"
+  },
+  {
+    "revision": "6edd37a588c97c4ee52c",
+    "url": "/GDL004-lab-notes/static/js/runtime-main.9112a11f.js"
+  },
+  {
+    "revision": "411051cb61bcda5517943601e8734cea",
+    "url": "/GDL004-lab-notes/static/media/Monoton-Regular.411051cb.ttf"
+  },
+  {
+    "revision": "7775b2ab8bdaa25788912b375b09d9f4",
+    "url": "/GDL004-lab-notes/static/media/Questrial-Regular.7775b2ab.ttf"
+  },
+  {
+    "revision": "0a7ca061367195d5b8b98df9cd0fb744",
+    "url": "/GDL004-lab-notes/static/media/googleLogo.0a7ca061.svg"
+  }
+]);
